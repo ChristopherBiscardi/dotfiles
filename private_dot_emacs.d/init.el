a9dc2f483e3d78dc622bfd9bb7ec858f9b37b80b
Christopher Biscardi <chris@christopherbiscardi.com>
2026-03-16 09:59:06 -0700

    emacs?

---
+++ private_dot_emacs.d/init.el
@@ -0,0 +1,15 @@
+(require 'org)
+(require 'org-tempo)
+(org-babel-load-file "~/.emacs.d/config.org")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
